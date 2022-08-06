f0f849a61da791215b1c32cce681b5a48e81fc97
emiliano38 <56303625+emiliano38@users.noreply.github.com>
2022-08-06 20:06:08 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanillahhh!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+var ordenesReceta = ["dddd","eee","ffff","ggg"];
+for (let i=0;i < ordenesReceta.length; i++)
+
+  console.log(ordenesReceta[i]);
